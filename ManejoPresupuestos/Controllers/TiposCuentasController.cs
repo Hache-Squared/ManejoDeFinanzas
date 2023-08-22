814f48a46a8635e9f58467c9160db6bef7ad077c
Gabriel Hdez <anlodx1@gmail.com>
2023-08-22 05:54:33 -0600

    Estamos Explorando las validaciones del formulario para asi desplegar alertas al usuario en un futuro

--- ManejoPresupuestos/Controllers/TiposCuentasController.cs
+++ ManejoPresupuestos/Controllers/TiposCuentasController.cs
@@ -14,6 +14,11 @@ namespace ManejoPresupuestos.Controllers
         [HttpPost]
         public IActionResult Crear(TipoCuenta tipoCuenta)
         {
+            if (!ModelState.IsValid)
+            {
+                return View(tipoCuenta);
+            }
+
             return View();
         }
     }
--- ManejoPresupuestos/Models/TipoCuenta.cs
+++ ManejoPresupuestos/Models/TipoCuenta.cs
@@ -1,8 +1,11 @@
-﻿namespace ManejoPresupuestos.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ManejoPresupuestos.Models
 {
     public class TipoCuenta
     {
         public int Id { get; set; }
+        [Required]
         public string Nombre { get; set; }
         public int UsuarioId { get; set; }
         public int Orden { get; set; }
