fb2e381be7748c6cf7f780879089e7cac2e6c53a
Gabriel Hdez <anlodx1@gmail.com>
2023-08-22 05:38:47 -0600

    Agregar archivos de proyecto.

---
+++ ManejoPresupuestos/Controllers/TiposCuentasController.cs
@@ -0,0 +1,20 @@
+﻿using ManejoPresupuestos.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ManejoPresupuestos.Controllers
+{
+    public class TiposCuentasController : Controller
+    {
+        
+        public IActionResult Crear()
+        {
+            return View();  
+        }
+
+        [HttpPost]
+        public IActionResult Crear(TipoCuenta tipoCuenta)
+        {
+            return View();
+        }
+    }
+}
---
+++ ManejoPresupuestos/Models/TipoCuenta.cs
@@ -0,0 +1,10 @@
+﻿namespace ManejoPresupuestos.Models
+{
+    public class TipoCuenta
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public int UsuarioId { get; set; }
+        public int Orden { get; set; }
+    }
+}
