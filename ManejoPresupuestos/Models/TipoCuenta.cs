26a0c7df003d46a28cd060dc2cbc2e6db802cf54
Gabriel Hdez <anlodx1@gmail.com>
2023-08-25 04:19:20 -0600

    Ejemplo de validaciones desde el front-end con librerias de jquery

--- ManejoPresupuestos/Models/TipoCuenta.cs
+++ ManejoPresupuestos/Models/TipoCuenta.cs
@@ -5,9 +5,29 @@ namespace ManejoPresupuestos.Models
     public class TipoCuenta
     {
         public int Id { get; set; }
-        [Required]
+        [Display(Name ="Nombre")]
+        [Required(ErrorMessage ="El campo {0} es requerido")]
+        [StringLength(maximumLength: 50, MinimumLength = 3, ErrorMessage = "El campo {0} debe estar entre {2} y {1} caracteres")]
         public string Nombre { get; set; }
         public int UsuarioId { get; set; }
         public int Orden { get; set; }
+
+
+        /*
+         Pruebas de validaciones por defecto
+         */
+        [Required(ErrorMessage ="{0} es requerido")]
+        [EmailAddress(ErrorMessage = "{0} No es valido")]
+        public string Email { get; set; }
+
+        [Range(minimum:18, maximum:130, ErrorMessage = "El valor debe ser entre {1} y {2}")]
+        public int Edad { get; set; }
+
+        [Url(ErrorMessage ="{0} No valida")]
+        public string Url { get; set; }
+
+        [Display(Name ="Tarjeta de Credito")]
+        [CreditCard(ErrorMessage ="Tarjeta no valida")]
+        public string TarjetaDeCredito { get; set; }
     }
 }
