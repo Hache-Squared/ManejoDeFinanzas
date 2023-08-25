20cda6f03a9353ddd6c0ca4a9acdcef89d164814
Gabriel Hdez <anlodx1@gmail.com>
2023-08-25 05:22:10 -0600

    Hemos hecho nuestra primera conexion con la base de datos

--- ManejoPresupuestos/Controllers/TiposCuentasController.cs
+++ ManejoPresupuestos/Controllers/TiposCuentasController.cs
@@ -1,13 +1,25 @@
-﻿using ManejoPresupuestos.Models;
+﻿using Dapper;
+using ManejoPresupuestos.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
 
 namespace ManejoPresupuestos.Controllers
 {
     public class TiposCuentasController : Controller
     {
-        
+        private readonly string connectionString;
+        public TiposCuentasController(IConfiguration configuration)
+        {
+            connectionString = configuration.GetConnectionString("DefaultConnection");
+        }
+
         public IActionResult Crear()
         {
+            using(var connection = new SqlConnection(connectionString))
+            {
+                var query = connection.Query("SELECT 1").FirstOrDefault();
+            }
+
             return View();  
         }
 
