e965e217fbca6143d980619574b3aa3d6ea00928
Gabriel Hernandez <aghh.oficial@gmail.com>
2023-09-03 23:23:18 -0600

    Podemos mostrar la vista de editar para Transacciones

---
+++ ManejoPresupuestos/wwwroot/js/FormularioTransacciones.js
@@ -0,0 +1,27 @@
+﻿function inicializarFormularioTransacciones(urlObtenerCategorias) {
+    $("#TipoOperacionId").change(async function () {
+        const valorSeleccionado = $(this).val();
+
+        const respuesta = await fetch(urlObtenerCategorias, {
+            method: "POST",
+            body: valorSeleccionado,
+            headers: {
+                'Content-type': 'application/json'
+            }
+        })
+
+        const json = await respuesta.json();
+        console.log(json)
+
+        if (!json) {
+            return;
+        }
+        const opciones = json.map(categoria => `<option value=${categoria.value}> ${categoria.text} </option>`);
+
+        $("#CategoriaId").html(opciones);
+
+
+
+
+    })
+}
