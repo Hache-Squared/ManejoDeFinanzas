f9643c9c7fad30f69dd6ffa7cdd674fb3d3a42cc
Gabriel Hernandez <aghh.oficial@gmail.com>
2023-09-27 04:38:56 -0600

    Hemos creado un formulario en la vista de registro del usuario

---
+++ ManejoPresupuestos/Controllers/UsuariosController.cs
@@ -0,0 +1,23 @@
+﻿using ManejoPresupuestos.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ManejoPresupuestos.Controllers
+{
+    public class UsuariosController : Controller
+    {
+        public IActionResult Registro()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Registro(RegistroViewModel modelo)
+        {
+            if(!ModelState.IsValid)
+            {
+                return View(modelo);
+            }
+
+            return RedirectToAction("Index", "Transacciones");
+        }
+    }
+}
---
+++ ManejoPresupuestos/Models/RegistroViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ManejoPresupuestos.Models
+{
+    public class RegistroViewModel
+    {
+        [Required(ErrorMessage = "El campo {0} es requerido")]
+        [EmailAddress(ErrorMessage = "El campo {0} debe ser un correo electronico valido")]
+        public string Email { get; set; }
+        [Required(ErrorMessage = "El campo {0} es requerido")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+}
